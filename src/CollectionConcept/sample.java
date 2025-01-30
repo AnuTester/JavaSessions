ff30280c8c424450e5717d98b88cebe4b9e11821
AnuTester <Anuradharkamath@gmail.com>
2025-01-30 11:49:59 -0800

    Create sample.java

---
+++ src/CollectionConcept/sample.java
@@ -0,0 +1,17 @@
+package CollectionConcept;
+
+import java.util.ArrayList;
+
+public class sample {
+   public void samplecode(){
+	   int  i = 9;
+		System .out.print(i);
+		ArrayList<String> list1 = new  ArrayList<String>();
+		for (i=0;i<9;i++) {list1.add(Integer.toString(i));}
+		System .out.print(list1);
+   }
+   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
