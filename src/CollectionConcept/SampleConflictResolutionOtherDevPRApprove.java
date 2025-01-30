1b1d2ab3dc248b3b3dab518da2d29ccf6442e608
AnuTester <Anuradharkamath@gmail.com>
2025-01-30 11:04:27 -0800

    Create SampleConflictResolutionOtherDevPRApprove.java

---
+++ src/CollectionConcept/SampleConflictResolutionOtherDevPRApprove.java
@@ -0,0 +1,16 @@
+package CollectionConcept;
+
+import java.util.ArrayList;
+
+public class SampleConflictResolutionOtherDevPRApprove {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int  i = 9;
+		System .out.print(i);
+		ArrayList<String> list1 = new  ArrayList<String>();
+		for (i=0;i<9;i++) {list1.add(Integer.toString(i));}
+		System .out.print(list1);
+	}
+
+}
