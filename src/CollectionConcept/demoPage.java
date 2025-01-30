048afd9413bf0f3cba709e98ac15a17143d3903b
Anu k <anuradharkamath@gmail.com>
2025-01-29 22:07:49 -0800

    demopage added

---
+++ src/CollectionConcept/demoPage.java
@@ -0,0 +1,11 @@
+package CollectionConcept;
+
+public class demoPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "test to add demo page " ; 
+		System.out.println(test);
+	}
+
+}
