4bd492e495d31e8a0a29f1fd319b784ef68d7b93
AnuTester <Anuradharkamath@gmail.com>
2025-01-30 20:07:32 -0800

    Create gitPullConcept.java

---
+++ src/CollectionConcept/gitPullConcept.java
@@ -0,0 +1,11 @@
+package CollectionConcept;
+
+public class gitPullConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "test to add git pull concept code " ; 
+		System.out.println(test);
+	}
+
+}
