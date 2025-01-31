d5321ee0799a0052a112e902e7d373c4de69ea09
AnuTester <Anuradharkamath@gmail.com>
2025-01-30 20:09:34 -0800

    Create gitFetchConcept.java

---
+++ src/CollectionConcept/gitFetchConcept.java
@@ -0,0 +1,11 @@
+package CollectionConcept;
+
+public class gitFetchConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "test to git fetch concept " ; 
+		System.out.println(test);
+	}
+
+}
